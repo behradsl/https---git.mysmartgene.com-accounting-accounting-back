ba9562aab3fa17a0899cbbdd93d0e9a9ece170ad
behrad soleymanian <behradsoleymanian@gmail.com>
2025-02-07 23:06:37 +0330

    feat: :sparkles: implement registry filed access interceptor

---
+++ src/common/FieldVisibility.interceptor.ts
@@ -0,0 +1,53 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Position } from '@prisma/client';
+import { Observable, map } from 'rxjs';
+import { OrmProvider } from 'src/providers/orm.provider';
+import { UserSessionType } from 'src/types/global-types';
+
+
+@Injectable()
+export class FieldVisibilityInterceptor implements NestInterceptor {
+  constructor(private readonly ormProvider: OrmProvider) {}
+
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+     
+    const session:UserSessionType = request.session;
+    const user = session.passport.user;
+    const userPosition = user.position;
+
+   
+    const allowedFields = await this.getAllowedFields(userPosition);
+
+    return next.handle().pipe(
+      map((data) => this.filterFields(data, allowedFields))
+    );
+  }
+
+  private async getAllowedFields(position: Position): Promise<string[]> {
+    const fieldAccess = await this.ormProvider.registryFieldAccess.findMany({
+      where: {
+        OR: [
+          { position, access: 'VISIBLE' },
+          { position, access: 'EDITABLE' }
+        ]
+      },
+      select: { registryField: true },
+    });
+        
+    return fieldAccess.map((fa) => fa.registryField);
+  }
+
+  private filterFields(data: any, allowedFields: string[]): any {
+    if (Array.isArray(data)) {
+      return data.map((item) => this.filterObject(item, allowedFields));
+    }
+    return this.filterObject(data, allowedFields);
+  }
+
+  private filterObject(obj: any, allowedFields: string[]): any {
+    if (!obj || typeof obj !== 'object') return obj; 
+    return Object.fromEntries(Object.entries(obj).filter(([key]) => allowedFields.includes(key)));
+  }
+  
+}
